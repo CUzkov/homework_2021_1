9a5ec079aec2c9052e5913aacca1df28c3da0214
CUzkov <UskovDanek@gmail.com>
2021-02-09 21:47:25 +0300

    Resolve conv

--- source/anagram.js
+++ source/anagram.js
@@ -1,33 +1,52 @@
-'use strict'
+'use strict';
 
+/**
+ * Сортировка массива из массивов строк по первому слову подмассива
+ * @function strSort
+ * @param {Array.<string>} a - первый массив
+ * @param {Array.<string>} b - второй массив
+ */
 const strSort = (a, b) => {
-    // В данном кейсе лаконичнее без {}
     if (a[0] > b[0]) return 1;
     if (a[0] < b[0]) return -1;
     return 0;
-}
+};
 
-let anagram = (words) => (
-    Array.isArray(words) ? words.reduce(
-        (acc, el) => {
-            const formatEl = el.split('').sort().join('');
-            if (acc[1].has(formatEl)) {
-                acc[0][acc[1].get(formatEl)].push(el);
-            } else {
-                acc[1].set(formatEl, acc[0].length);
-                acc[0].push([el]);
-            }
-            return acc;
-        }, 
-        [[], new Map()]
-    )[0].reduce(
-        (acc, el) => {
-            if (el.length > 1) {
-                acc.push(el.sort())
-            }
-            return acc;
-        }, 
-        []
-    // тест не очень, смылс сортировки по первому слову....
-    ).sort(strSort) : null
-)
+/**
+ * Получение массива массивов анаграмм
+ * @param {Array.<Array<string> | Map.<string, number>>} acc - аккумулятор для reduce
+ * @param {string} el - слово
+ */
+const getAnagramArr = (acc, el) => {
+    const formatEl = el.split('').sort().join('');
+    if (acc[1].has(formatEl)) {
+        acc[0][acc[1].get(formatEl)].push(el);
+    } else {
+        acc[1].set(formatEl, acc[0].length);
+        acc[0].push([el]);
+    }
+    return acc;
+};
+
+/**
+ * Фильтрация массива анаграмм
+ * @function filterAndSortAnagrams
+ * @param {Array.<Array<string>>} acc - аккумулятор для reduce
+ * @param {Array<string>} author - очередной элемент
+ */
+const filterAndSortAnagrams = (acc, el) => {
+    if (el.length > 1) {
+        acc.push(el.sort());
+    }
+    return acc;
+};
+
+/**
+ * Получение отсортированного массива массивов анаграмм, последние из которых состоят больше чем из 1 слова
+ * @function anagram
+ * @param {Array.<string>} words - Массив слов
+ */
+const anagram = (words) =>
+    Array.isArray(words)
+        ? words.reduce(getAnagramArr, [[], new Map()])[0].reduce(filterAndSortAnagrams, []).sort(strSort)
+        : null;
